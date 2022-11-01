d054280abd2a7b6c0e713185647ffe122e0a33bf
Shivram <sr31415@outlook.com>
2022-11-01 13:40:32 -0400

    Add font details

--- JustInTime Mobile App/screens/Home/index.js
+++ JustInTime Mobile App/screens/Home/index.js
@@ -3,11 +3,11 @@ import { View, Text } from 'react-native';
 export default Home = () => (
     <View
         style={{
-            flexDirection: 'row',
-            height: 100,
             padding: 20,
         }}
     >
-        <Text>Base content</Text>
+        <Text style={{ fontSize: 24, color: '#4B9CD3' }}>
+            Tuesday, November 1
+        </Text>
     </View>
 );
