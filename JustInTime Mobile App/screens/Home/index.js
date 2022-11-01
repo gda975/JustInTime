aa6a7e63092264f39fa9b7f9017be6abd7e3de35
Shivram <sr31415@outlook.com>
2022-11-01 13:34:36 -0400

    Establish base SafeAreaView

---
+++ JustInTime Mobile App/screens/Home/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+export default Home = () => (
+    <View
+        style={{
+            flexDirection: 'row',
+            height: 100,
+            padding: 20,
+        }}
+    >
+        <Text>Base content</Text>
+    </View>
+);
