f7f941e0ed4f5a53b947430f595cac4dec20bc91
anhhuynhUNC <anhhuynh@live.unc.edu>
2022-11-03 12:52:44 -0400

    added atom

--- admin-app/src/Atom.js
+++ admin-app/src/Atom.js
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
-export default function Atom(props){
+export default function Atom(props) {
     let [toggleEdit, setEdit] = useState(false);
+    let textEl = useRef(null);
 
-    function toggleEditFunc(){
+    useEffect(() => {
+        if (textEl.current) {
+            textEl.current.focus();
+
+            //move cursor
+            textEl.current.selectionStart = textEl.current.value.length;
+            textEl.current.selectionEnd = textEl.current.value.length;
+        }
+    }, [toggleEdit])
+
+    function toggleEditFunc() {
         setEdit(!toggleEdit);
     }
 
@@ -11,12 +22,13 @@ export default function Atom(props){
         <div className="content-page">
             <p>Author: {props.val[1].author}</p>
             <div className="content" key={props.index + ""}>
-                
-                {!toggleEdit ? <div className="content-actual"> Content: {props.val[1] == undefined ? "" : props.val[1].content} </div> : 
-                <div className="content-edit">
-                    <textarea id = "content-txt" defaultValue={props.val[1] == undefined ? "" : props.val[1].content}></textarea>    
-                </div>}
-                
+
+                {!toggleEdit ? <div className="content-actual"> Content: {props.val[1] == undefined ? "" : props.val[1].content} </div> :
+                    <div className="content-edit">
+                        <textarea ref={textEl} id="content-txt" defaultValue={props.val[1] == undefined ? "" : props.val[1].content}></textarea>
+                        <button> save </button>
+                    </div>}
+
             </div>
             <p>Time: {props.val[1].time}</p>
 
