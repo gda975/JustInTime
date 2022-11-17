fcbafa6721bae4955a47f33b4c7ca27a87a15a9d
gda975 <gda@ad.unc.edu>
2022-11-17 01:06:33 -0500

    Added a content page

--- JustInTime Mobile App/src/navigation/index.js
+++ JustInTime Mobile App/src/navigation/index.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 // Screens
@@ -10,7 +10,8 @@ import HomeScreen from './screens/Home';
 import UpdatesScreen from './screens/Updates';
 import ResourcesScreen from './screens/Resources';
 import SettingsScreen from './screens/Settings';
-
+import ContentScreen from './screens/Content';
+import HomeStack from './stacks/HomeStack';
 
 // Screen Names
 const homeName = 'Home';
@@ -18,48 +19,55 @@ const updatesName = 'Updates';
 const resourcesName = 'Resources';
 const settingsName = 'Settings';
 
-
 const Tab = createBottomTabNavigator();
 
 const MainContainer = () => {
     return (
         <NavigationContainer>
             <Tab.Navigator
-            initialRouteName={homeName}
-            screenOptions={({route}) => ({
-                tabBarIcon: ({focused, color, size}) => {
-                    let iconName;
-                    let routeName = route.name;
+                initialRouteName={homeName}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        let routeName = route.name;
 
-                    if (routeName === homeName) {
-                        iconName = focused ? 'home' : 'home-outline'
-                    } else if (routeName === updatesName) {
-                        iconName = focused ? 'sync' : 'sync-outline'
-                    } else if (routeName === resourcesName) {
-                        iconName = focused ? 'albums' : 'albums-outline'
-                    } else if (routeName === settingsName) {
-                        iconName = focused ? 'settings' : 'settings-outline'
-                    }
+                        if (routeName === homeName) {
+                            iconName = focused ? 'home' : 'home-outline';
+                        } else if (routeName === updatesName) {
+                            iconName = focused ? 'sync' : 'sync-outline';
+                        } else if (routeName === resourcesName) {
+                            iconName = focused ? 'albums' : 'albums-outline';
+                        } else if (routeName === settingsName) {
+                            iconName = focused
+                                ? 'settings'
+                                : 'settings-outline';
+                        }
 
-                    return <Ionicons name={iconName} size={size} color={color}/>
-                },
-                tabBarActiveTintColor: '#FFFFFF',
-                tabBarInactiveTintColor: '#000000',
-                tabBarStyle: {padding: 10, height: 85, backgroundColor: '#5DADE2'},
-                headerShown: false
-
-            })}
+                        return (
+                            <Ionicons
+                                name={iconName}
+                                size={size}
+                                color={color}
+                            />
+                        );
+                    },
+                    tabBarActiveTintColor: '#FFFFFF',
+                    tabBarInactiveTintColor: '#000000',
+                    tabBarStyle: {
+                        padding: 10,
+                        height: 85,
+                        backgroundColor: '#5DADE2',
+                    },
+                    headerShown: false,
+                })}
             >
-
-            <Tab.Screen name={homeName} component={HomeScreen}/>
-            <Tab.Screen name={updatesName} component={UpdatesScreen}/>
-            <Tab.Screen name={resourcesName} component={ResourcesScreen}/>
-            <Tab.Screen name={settingsName} component={SettingsScreen}/>
-
-
+                <Tab.Screen name={homeName} component={HomeStack} />
+                <Tab.Screen name={updatesName} component={UpdatesScreen} />
+                <Tab.Screen name={resourcesName} component={ResourcesScreen} />
+                <Tab.Screen name={settingsName} component={SettingsScreen} />
             </Tab.Navigator>
         </NavigationContainer>
-    )
-}
+    );
+};
 
 export default MainContainer;
