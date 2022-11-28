063553d5d06de485678b48a470245de2b2b2cb61
Shivram <sr31415@outlook.com>
2022-11-27 20:31:13 -0500

    Adjust test

--- JustInTime Mobile App/src/__tests__/Home.test.js
+++ JustInTime Mobile App/src/__tests__/Home.test.js
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react-native';
 import HomeScreen from './../../src/navigation/screens/Home';
-import App from './../navigation/index.js';
 
 test('first test', () => {
-    jest.mock('Ionicons', () => jest.fn());
-    render(<App />);
-    // render(<HomeScreen />);
+    render(<HomeScreen />);
     expect(screen.getByText('Feed')).toBeTruthy();
-    expect(screen.getByText('Settings')).toBeTruthy();
+    expect(
+        screen.getByText(`${new Date().getDate()}`, {
+            exact: false,
+        })
+    ).toBeTruthy();
 });
