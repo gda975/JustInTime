9fd22a2adad96df8731f3e881b08ac52fab80344
Shivram <sr31415@outlook.com>
2022-11-28 23:29:03 -0500

    Add Settings page UI

--- JustInTime Mobile App/src/__tests__/Settings.test.js
+++ JustInTime Mobile App/src/__tests__/Settings.test.js
@@ -4,12 +4,12 @@ import renderer from 'react-test-renderer';
 
 jest.useFakeTimers();
 
-test('integration check that title shows on resources page', () => {
+test('integration check that title shows on settings page', () => {
     render(<SettingsScreen />);
     expect(screen.getByText('Settings')).toBeTruthy();
 });
 
-test('check that date bar shows on resources page', () => {
+test('check that date bar shows on settings page', () => {
     render(<SettingsScreen />);
     expect(
         screen.getByText(`${new Date().getDate()}`, {
@@ -18,7 +18,18 @@ test('check that date bar shows on resources page', () => {
     ).toBeTruthy();
 });
 
-test('check for same updates page snapshot', () => {
+test('check notifications settings render properly', () => {
+    render(<SettingsScreen />);
+    expect(screen.getByText('Allow notifications')).toBeTruthy();
+    expect(screen.getByText('Subscribed notifications:')).toBeTruthy();
+});
+
+test('check feed settings render properly', () => {
+    render(<SettingsScreen />);
+    expect(screen.getByText('Show in feed:')).toBeTruthy();
+});
+
+test('check for same settings page snapshot', () => {
     const tree = renderer.create(<SettingsScreen />).toJSON();
     expect(tree).toMatchSnapshot();
 });
