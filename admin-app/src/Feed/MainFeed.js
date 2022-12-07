c4925ff9b3d67ff194d7a135eead601fd0e16ad0
Shivram <sr31415@outlook.com>
2022-12-06 19:27:41 -0500

    Fix UNC typo

--- admin-app/src/Feed/MainFeed.js
+++ admin-app/src/Feed/MainFeed.js
@@ -3,7 +3,7 @@ import Atom from '../AtomMain/Atom';
 export default function MainFeed(props) {
     return (
         <div className="sub-main-container">
-            <h1>Center for Nursing Excellence</h1>
+            <h1>UNC Center for Nursing Excellence</h1>
             <h2>Just in Time Feed</h2>
             <h2>{props.category}</h2>
             {props.insertButton}
