09f71920f5568adf2c27c1d13d1657c3e11ff24d
Andrew Byerle <andran@live.unc.edu>
2023-02-22 16:34:40 -0500

    fcm console handling

--- JustInTime Mobile App/App.js
+++ JustInTime Mobile App/App.js
@@ -4,6 +4,7 @@ import 'intl';
 import 'intl/locale-data/jsonp/en';
 import messaging from '@react-native-firebase/messaging';
 import { useEffect } from 'react';
+import { Alert } from 'react-native';
 
 export default function App() {
     async function requestUserPermission() {
@@ -24,6 +25,29 @@ export default function App() {
         } else{
             console.log("failed to get token")
         }
+
+        messaging()
+          .getInitialNotification()
+          .then(remoteMessage => {
+            if (remoteMessage){
+              console.log('notification caused app to open from quit state:', remoteMessage.notification)
+            }
+          })
+
+          messaging().onNotificationOpenedApp(async (remoteMessage) => {
+            console.log('Notification caused app to open from background state:', remoteMessage.notification)
+          })
+
+          messaging().setBackgroundMessageHandler(async remoteMessage => {
+            console.log('Message handled in the background', remoteMessage)
+          })
+
+          // in app alert because only option supported
+          const unsubscribe = messaging().onMessage(async remoteMessage => {
+            Alert.alert('a new message arrived', JSON.stringify(remoteMessage))
+          })
+
+          return unsubscribe;
     })
     return <MainContainer />;
 }
